de65fb5307d2204f1c7bf41853652b53c39a5c6a
Sergey <saizotov1@gmail.com>
2023-07-13 00:47:27 +0500

    first commit

--- Lesson_2_1.java
+++ Lesson_2_1.java
@@ -1,27 +1,27 @@
 import java.io.IOException;
-import java.util.logging.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
 public class Lesson_2_1 {
-    private static Object args;
-
+    public static <args> void main() throws IOException {
+        Logger logger = Logger.getLogger(Lesson_2_1.class.getName());
+        FileHandler fh = new FileHandler("logLesson_2_1.xml");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
+        logger.info("Массив: ");
+        int [] array = {2, 6, 4, -8, 3, 46, 0};
+        printArrayInt(array);
+        int[] newArr = listSort(array);
+        System.out.printf("\n:Отсортированный массив: \n");
+        printArrayInt(newArr);
+    }
     public static void printArrayInt(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
             System.out.printf("%d, ", arr[i]);
         }
             System.out.print(arr[arr.length-1]);
-   public static void main(String[], args) throws IOException {
-            Logger logger = Logger.getLogger(Lesson_2_1.class.getName());
-            FileHandler fh = new FileHandler("logLesson_2_1.xml");
-            logger.addHandler(fh);
-            XMLFormatter xml = new XMLFormatter();
-            fh.setFormatter(xml);
-            logger.info("Массив: ");
-            int [] array = {2, 6, 4, -8, 3, 46, 0};
-            printArrayInt(array);
-            int[] newArr = listSort(array);
-            System.out.printf("\n:Отсортированный массив: \n");
-            printArrayInt(newArr);
-        }
     }
     public static int[] listSort(int [] arr){
         for(int k = 0; k < arr.length-1; k++) {
--- Lesson_3_1.java
+++ Lesson_3_1.java
@@ -1,10 +1,31 @@
 import java.util.ArrayList;
 
 public class Lesson_3_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        array = FillRandomArray(10);
+        PrintArray(array);
+        DeleteEvenNumber(array);
+        PrintArray(array);
+    }
     public static void PrintArray(ArrayList<Integer> arr) {
-        for(int element: arr) {
+        for (int element : arr) {
             System.out.printf("%d, ", element);
         }
         System.out.println();
 }
+    public static ArrayList<Integer> FillRandomArray(int n) {
+        ArrayList <Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            list.add((int)(Math.random()*100));
+        }
+        return list;
+    }
+    public static void DeleteEvenNumber(ArrayList<Integer> list){
+        int i = 0;
+        while (i < list.size()){
+            if(list.get(i) % 2 == 0) list.remove(i);
+            else i++;
+        }
+    }
 }
